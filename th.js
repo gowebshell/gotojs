d9e91d33cbf6e2140c8ab3d8dd3be3b8c8f97659
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-10-08 01:45:11 -0300

    Create th.js

---
+++ th.js
@@ -0,0 +1,2 @@
+document.write('<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>');
+document.write('<script>LA.init({id:"3GA0lNgcmuQlTni3",ck:"3GA0lNgcmuQlTni3"})</script>');
