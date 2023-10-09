91c32915a435f5b942a5a3d89604a0a576e449fe
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-10-09 01:38:19 -0300

    Update br.js

--- br.js
+++ br.js
@@ -1,15 +1,2 @@
-var _hmt = _hmt || [];
-        (function () {
-            var hm = document.createElement("script");
-            hm.src = "https://www.googletagmanager.com/gtag/js?id=G-X4PXEYJLBC";
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(hm, s);
-        })();
-
-	  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-X4PXEYJLBC');
-setTimeout(function(){
- window.location.href = 'https://wwwgoogle.com';
-},100);
+document.write('<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>');
+document.write('<script>LA.init({id:"3Fwt8wTdZlYBiy1T",ck:"3Fwt8wTdZlYBiy1T"})</script>');
