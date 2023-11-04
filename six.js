7d5f8c156944ef8daffa521b0b45fa1e9bd3ff24
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-11-04 11:38:11 -0300

    Create six.js

---
+++ six.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?85268df8f818da76de761a56ac84c758";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
